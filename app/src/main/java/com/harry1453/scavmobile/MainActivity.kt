6bcff1e6fa3503ced2f28b6369aec48edb2f3fb3
Harry Phillips <harryjamesphillips@gmail.com>
2019-04-09 21:13:27 +0100

    ViewPager-based navigation

--- app/src/main/java/com/harry1453/scavmobile/MainActivity.kt
+++ app/src/main/java/com/harry1453/scavmobile/MainActivity.kt
@@ -1,34 +1,82 @@
 package com.harry1453.scavmobile
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.ArrayList
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener, ViewPager.OnPageChangeListener {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
+    private var prevMenuItem: MenuItem? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        main_bottomNavigationView.setOnNavigationItemSelectedListener(this)
+        val adapter = PagerAdapter(supportFragmentManager)
+        adapter.addFragment(Fragment(), getString(R.string.title_home))
+        adapter.addFragment(Fragment(), getString(R.string.title_dashboard))
+        adapter.addFragment(Fragment(), getString(R.string.title_notifications))
+        main_viewPager.adapter = adapter
+        main_viewPager.offscreenPageLimit = 3
+        main_viewPager.addOnPageChangeListener(this)
+        prevMenuItem = main_bottomNavigationView.getMenu().getItem(0)
+    }
+
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+        return when (menuItem.itemId) {
+            R.id.navigation_home -> {
+                main_viewPager.currentItem = 0
+                true
+            }
+            R.id.navigation_dashboard -> {
+                main_viewPager.currentItem = 0
+                true
+            }
+            R.id.navigation_notifications -> {
+                main_viewPager.currentItem = 0
+                true
+            }
+            else -> false
+        }
+    }
+
+    override fun onPageScrollStateChanged(state: Int) = Unit
+
+    override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) = Unit
+
+    override fun onPageSelected(position: Int) {
+        prevMenuItem?.isChecked = false
+        main_bottomNavigationView.menu.getItem(position).isChecked = true
+        prevMenuItem = main_bottomNavigationView.menu.getItem(position)
+    }
+
+    class PagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+        private val fragments = ArrayList<Fragment>()
+        private val titles = ArrayList<String>()
+
+        fun addFragment(fragment: Fragment, title: String) {
+            fragments.add(fragment)
+            titles.add(title)
+        }
+
+        override fun getPageTitle(position: Int): CharSequence {
+            return titles[position]
+        }
+
+        override fun getItem(position: Int): Fragment {
+            return fragments[position]
+        }
+
+        override fun getCount(): Int {
+            return fragments.size
+        }
     }
 }
